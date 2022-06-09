3ac7f00ad47b6620c7f572f3534ec8e281f94d4f
Lucca Rauédys <luccarauedys@outlook.com>
2022-06-09 15:09:36 -0300

    preparando para o deploy

--- src/config/database.js
+++ src/config/database.js
@@ -3,29 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pg;
-const db = new Pool({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const db = new Pool(databaseConfig);
 
 export default db;
-
-// import pg from "pg";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const { Pool } = pg;
-
-// const databaseConfig = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// };
-
-// const db = new Pool(databaseConfig);
-
-// export default db;
